b2bb02b2bc07ed5c1170da8592f5fc65b4ecf0f9
Samayamanthula Navya Sai Narayana Datta <samayamantula.saidatta@gmail.com>
2022-05-26 00:05:34 +0530

    Final Commit

--- caluculator.js
+++ caluculator.js
@@ -1,80 +1,89 @@
-let showValues = document.getElementById('show-values');
-let left = '',right = '',evaluator = '';
-function clearScreen() {
-  showValues.innerText = 0;
+let buffer = "0";
+let runningTotal = 0;
+let previousOperator = null;
+
+const screen = document.querySelector(".screen");
+
+function buttonClick(value){
+  if(isNaN(value)){
+    handleSymbol(value);
+  } else{
+    handleNumbers(value);
+  }
+  rerender();
 }
-function clearLastElement(){
-  let length = showValues.innerText.length;
-  showValues.innerText = showValues.innerText.slice(0, length-1)
-  if(length <= 1){
-    showValues.innerText = 0;
+
+function handleNumbers(value){
+  if(buffer === "0"){
+    buffer = "" + value;
+  } else {
+    buffer += value;
   }
 }
 
+function handleSymbol(value){
+  switch(value){
+    case 'C':
+      buffer = "0";
+      runningTotal = 0;
+      break;
+    case '=':
+      if(previousOperator == null){
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = +runningTotal;
+      runningTotal = 0;
+      break;
+    case "←":
+      if(buffer.length === 1){
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length-1)
+      }
+      break;
+    default:
+      handleMath(value);
+      break;
+  }
+}
 
-let firstRowValues = document.querySelector('#first-row-container').addEventListener('click', function(event) {
-  if(event.target.className === 'calc-button') {
-    if(event.target.innerText === "/"){
-      evaluator = event.target.innerText
-      left = showValues.innerText;
-      showValues.innerText = 0;
-    }
+function handleMath(value){
+  if(buffer === "0"){
+    return;
+  } 
+  const intBuffer = parseInt(buffer);
+  if(runningTotal === 0){
+    runningTotal = intBuffer;
+  } else{
+    flushOperation(intBuffer);
   }
-})
+  previousOperator = value;
+  buffer = "0";
 
-let secondRowValues = document.querySelector('#second-row-container').addEventListener('click', function(event) {
-  if(event.target.className === 'calc-button') {
-    if(event.target.innerText === "*"){
-      evaluator = event.target.innerText
-      left = showValues.innerText;
-      showValues.innerText = 0;
-    }
-    else{
-      showValues.innerText += event.target.innerText;
-    }
-  }
-})
+}
 
-let thirdRowValues = document.querySelector('#third-row-container').addEventListener('click', function(event) {
-  if(event.target.innerText === "-"){
-    evaluator = event.target.innerText
-    left = showValues.innerText;
-    showValues.innerText = 0;
+function flushOperation(intBuffer){
+  if(previousOperator == '+'){
+    runningTotal += intBuffer;
+  } else if(previousOperator == '-'){
+    runningTotal -= intBuffer;
+  } else if(previousOperator == '*'){
+    runningTotal *= intBuffer;
+  } else if(previousOperator == '/'){
+    runningTotal /= intBuffer;
   }
-  else{
-    showValues.innerText += event.target.innerText;
-  }
-})
+}
 
-let fourthRowValues = document.querySelector('#fourth-row-container').addEventListener('click', function(event) {
-  if(event.target.innerText === "+"){
-    evaluator = event.target.innerText
-    left = showValues.innerText;
-    showValues.innerText = 0;
-  }
-  else{
-    showValues.innerText += event.target.innerText;
-  }
-})
+function rerender() {
+  screen.innerText = buffer;
+}
 
-let fifthRowValues = document.querySelector('#fifth-row-container').addEventListener('click', function(event) {
-  if(event.target.innerText === "="){
-    right = showValues.innerText;
-    if(evaluator == '+'){
-      showValues.innerText = parseInt(left) + parseInt(right);
-    }
-    else if(evaluator == '-'){
-      showValues.innerText = parseInt(left) - parseInt(right);
-    }
-    else if(evaluator == '*'){
-      showValues.innerText = parseInt(left) * parseInt(right);
-    }
-    else if(evaluator == '/'){
-      showValues.innerText = (parseInt(left) / parseInt(right)).toFixed(4);
-    }
-  }
-  else{
-    showValues.innerText += event.target.innerText;
-  }
-})
+function init(){
+  document.querySelector(".calc-buttons").addEventListener("click", function(event) {
+    buttonClick(event.target.innerText);
+  });
+}
 
+init();
